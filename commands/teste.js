30c1d71375d845ea15ff49976a3235df86e80bd9
Arthur Felipe <64918853+Dev-Atsushi@users.noreply.github.com>
2020-09-25 09:29:08 -0300

    Create teste.js

---
+++ commands/teste.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js")
+module.exports.run = async (client, message, args) => {
+
+message.channel.send("Teste | Copyright Atsushi 🪐#0012")
+
+}
+module.exports.info = {
+name: "teste"
+}
