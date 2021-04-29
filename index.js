93c4c6c825c94eb470ce9141a5578f7f66e2e422
Atsushi <atsushithesushi@gmail.com>
2021-04-29 09:29:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
-const config = require("./config.js");
-let fs = require("fs")
+const client  = new Discord.Client();
+const config  = require("./config.js");
+let fs = require("fs");
+
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
+
 fs.readdir(`./commands/`, (err, files) => {
     if (err) console.error(err);
 
@@ -11,8 +13,8 @@ fs.readdir(`./commands/`, (err, files) => {
     if (fileCommand.length <= 0) return;
     fileCommand.forEach((fileCommand, i) => {
         let props = require(`./commands/${fileCommand}`);
-        console.log(props.info.name)
-        client.commands.set(props.info.name, props)
+        console.log(props.info.name);
+        client.commands.set(props.info.name, props);
         if (!props.info.aliases) return;
         props.info.aliases.forEach(alias => {
             client.aliases.set(alias, props.info.name);
@@ -27,13 +29,13 @@ client.on("message", async message => {
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
     const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command));
-    if(!cmd)
+    if(!cmd);
 
     cmd.run(client, message, args);
-})
+});
 
 client.on("ready", () => {
-console.log("Bot Online!")
+    console.log("Bot Online!");
 });
 
 client.login(config.token);
